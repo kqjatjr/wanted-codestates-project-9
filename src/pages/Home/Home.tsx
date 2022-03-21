0ef1a5c40841dbdbd3ae2be5e05425a38dec9768
김범석 <kqjatjr@gmail.com>
2022-03-21 23:31:07 +0900

    home 구현

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,30 @@
+import Menu from "../../components/Menu/Menu";
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Menu />
+      <div className={styles.searchContainer}>
+        <p className={styles.titleB}>KartRider RECORD</p>
+      </div>
+      <div className={styles.inputContainer}>
+        <input />
+      </div>
+      <img
+        className={styles.dao}
+        src="	https://tmi.nexon.com/img/assets/covid_right.png"
+        alt="다오"
+      />
+      <img
+        className={styles.bazzi}
+        src="https://tmi.nexon.com/img/assets/covid_left.png"
+        alt="배찌"
+      />
+      <span className={styles.leftArrow} />
+      <span className={styles.rightArrow} />
+    </div>
+  );
+};
+
+export default Home;
