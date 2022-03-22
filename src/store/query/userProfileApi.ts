2368df58711f09934b22b4914120628783e8d355
김범석 <kqjatjr@gmail.com>
2022-03-22 14:41:11 +0900

    유저 개인 전적 Query구현

---
+++ src/store/query/userProfileApi.ts
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type TUser = {
+  accessId: string;
+  level: number;
+  name: string;
+};
+
+const USER_PROFILE = "userProfileApi";
+
+export const userProfileApi = createApi({
+  reducerPath: USER_PROFILE,
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "Authorization",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTcyODYwMjc1NCIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTM5MyIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2NDc4NDA5MDcsImV4cCI6MTY2MzM5MjkwNywiaWF0IjoxNjQ3ODQwOTA3fQ.2s7Rl9woR18ih1mAW7v8I07BEmG7KojARKvmCcEq_HE",
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUserAccessId: builder.query<TUser, string>({
+      query: (nickname) => `/users/nickname/${nickname}`,
+    }),
+    getUserMatchesList: builder.query({
+      query: (access_id) =>
+        `/users/${access_id}/matches?start_date=2022-03-01&end_date=2022-03-31&limit=100`,
+    }),
+  }),
+});
+
+export const { useGetUserAccessIdQuery, useGetUserMatchesListQuery } =
+  userProfileApi;
