5eb837d66481866b77c764f2cc17aeca26866298
김범석 <kqjatjr@gmail.com>
2022-03-24 03:05:30 +0900

    모든 매치리스트 및 특정 매치타입 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -55,3 +55,40 @@ export type TUserMatches = {
   matches: TMatches[];
   nickName: string;
 };
+
+export type TAllMatches = {
+  matchType: string;
+  matches: string[];
+};
+
+export type TTeam = {
+  accountNo: string;
+  characterName: string;
+  character: string;
+  kart: string;
+  license: string;
+  pet: string;
+  flyingPet: string;
+  partsEngine: string;
+  partsHandle: string;
+  partsWheel: string;
+  partsKit: string;
+  rankinggrade2: string;
+  matchRank: string;
+  matchRetired: string;
+  matchWin: string;
+  matchTime: string;
+};
+
+export type TTargetMatch = {
+  channelName: string;
+  startTime: string;
+  endTime: string;
+  gameSpeed: number;
+  matchId: string;
+  matchResult: string;
+  matchType: string;
+  playTime: number;
+  trackId: string;
+  teams: TTeam[] | TPlayer[];
+};
