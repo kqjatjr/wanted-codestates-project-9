5ec35d36163ab15ab90cea4243bd4e1fc0eaa5c3
김범석 <kqjatjr@gmail.com>
2022-03-24 03:10:50 +0900

    메뉴 구현

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,18 +1,66 @@
+import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
+import CustomLink from "../CustomLink/CustomLink";
 import styles from "./Menu.module.scss";
 
-const menuList: string[] = ["홈", "랭킹", "트랙"];
+const menuList: string[] = ["Home", "Rank", "Track"];
 
-const Menu = () => {
+type TProp = {
+  currMenu: string;
+  onChangeMenu: React.Dispatch<React.SetStateAction<string>>;
+  onSearchUser: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onChangeInputValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Menu = ({
+  currMenu,
+  onChangeMenu,
+  onSearchUser,
+  onChangeInputValue,
+}: TProp) => {
   return (
     <div className={styles.container}>
       <ul className={styles.menuList}>
         {menuList.map((name) => {
+          if (name === "Home") {
+            return (
+              <li key={name}>
+                <CustomLink to={`/`}>{name}</CustomLink>
+              </li>
+            );
+          }
+
           return (
             <li key={name}>
-              <div className={styles.menuName}>{name}</div>
+              {/* <div
+                onClick={() => onClickMenu(name)}
+                className={styles.menuName}
+                style={
+                  currMenu === name
+                    ? {
+                        borderBottom: "4px solid #fff",
+                        color: "#fff",
+                      }
+                    : {}
+                }
+              >
+                {name}
+              </div> */}
+              <CustomLink to={`/` + name}>{name}</CustomLink>
             </li>
           );
         })}
+        {currMenu !== "홈" && (
+          <li>
+            <div className={styles.inputContainer}>
+              <input
+                className={styles.input}
+                onChange={onChangeInputValue}
+                onKeyPress={onSearchUser}
+                placeholder="유저 닉네임을 입력후 Enter를 눌러주세요"
+              />
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
