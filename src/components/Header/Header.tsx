90708d9e59a8cae32c57aa877b9d4bc6ed838488
김범석 <kqjatjr@gmail.com>
2022-03-25 22:19:06 +0900

    헤더 a태그 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <img
-        className={styles.kartLogo}
-        src="https://tmi.nexon.com/img/assets/logo_kart.png"
-        alt="카트라이더 로고"
-      />
-      <div className={styles.moveSite}>카트라이더 홈페이지 바로가기</div>
+      <a href="https://kart.nexon.com/main/index.aspx">
+        <img
+          className={styles.kartLogo}
+          src="https://tmi.nexon.com/img/assets/logo_kart.png"
+          alt="카트라이더 로고"
+        />
+      </a>
+
+      <div className={styles.moveSite}>
+        <a href="https://kart.nexon.com/main/index.aspx">
+          카트라이더 홈페이지 바로가기
+        </a>
+      </div>
     </div>
   );
 };
